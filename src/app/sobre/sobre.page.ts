17f1885c5df036093bbd4fa80849ab1d201a0c41
RafaelMDev29 <l.rafael29@outlook.com>
2025-09-07 16:35:26 -0300

    Formadora 1

--- src/app/sobre/sobre.page.ts
+++ src/app/sobre/sobre.page.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-sobre',
+  templateUrl: './sobre.page.html',
+  styleUrls: ['./sobre.page.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule, RouterModule],
 })
-export class HomePage {
+export class SobrePage {
   constructor() {}
 }
